c5a9e0692223573f4445cc0b9c063c506799cd95
ShayestehHS <hosseinshayesteh47.hs@gmail.com>
2021-02-01 17:22:09 +0330

    Version 1

---
+++ BotManager/frmMain.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace BotManager
+{
+    partial class frmBotManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.lblToken = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.dgvLog = new System.Windows.Forms.DataGridView();
+            this.ChatID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UserName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Command = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MessageID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtCaption = new System.Windows.Forms.TextBox();
+            this.lblCaption = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblChannelID = new System.Windows.Forms.Label();
+            this.txtChannelID = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLog)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtToken
+            // 
+            this.txtToken.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtToken.Location = new System.Drawing.Point(64, 19);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(438, 27);
+            this.txtToken.TabIndex = 0;
+            this.txtToken.Text = "1607543553:AAHFfLycuygkUa-1jVAPybMSzzJGabuHU2s";
+            // 
+            // lblToken
+            // 
+            this.lblToken.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblToken.Location = new System.Drawing.Point(8, 23);
+            this.lblToken.Name = "lblToken";
+            this.lblToken.Size = new System.Drawing.Size(100, 23);
+            this.lblToken.TabIndex = 1;
+            this.lblToken.Text = "Token:";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 486);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.statusStrip1.Size = new System.Drawing.Size(848, 29);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(52, 23);
+            this.lblStatus.Text = "Offline";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(691, 456);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(145, 27);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Turn On your Bot";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // dgvLog
+            // 
+            this.dgvLog.AllowUserToAddRows = false;
+            this.dgvLog.AllowUserToDeleteRows = false;
+            this.dgvLog.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvLog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLog.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ChatID,
+            this.UserName,
+            this.Command,
+            this.MessageID,
+            this.Date});
+            this.dgvLog.Location = new System.Drawing.Point(12, 52);
+            this.dgvLog.Name = "dgvLog";
+            this.dgvLog.ReadOnly = true;
+            this.dgvLog.RowHeadersWidth = 51;
+            this.dgvLog.RowTemplate.Height = 24;
+            this.dgvLog.Size = new System.Drawing.Size(824, 256);
+            this.dgvLog.TabIndex = 4;
+            // 
+            // ChatID
+            // 
+            this.ChatID.DataPropertyName = "ChatID";
+            this.ChatID.HeaderText = "ChatID";
+            this.ChatID.MinimumWidth = 6;
+            this.ChatID.Name = "ChatID";
+            this.ChatID.ReadOnly = true;
+            // 
+            // UserName
+            // 
+            this.UserName.DataPropertyName = "UserName";
+            this.UserName.HeaderText = "UserName";
+            this.UserName.MinimumWidth = 6;
+            this.UserName.Name = "UserName";
+            this.UserName.ReadOnly = true;
+            // 
+            // Command
+            // 
+            this.Command.DataPropertyName = "Command";
+            this.Command.HeaderText = "Command";
+            this.Command.MinimumWidth = 6;
+            this.Command.Name = "Command";
+            this.Command.ReadOnly = true;
+            // 
+            // MessageID
+            // 
+            this.MessageID.DataPropertyName = "MessageID";
+            this.MessageID.HeaderText = "MessageID";
+            this.MessageID.MinimumWidth = 6;
+            this.MessageID.Name = "MessageID";
+            this.MessageID.ReadOnly = true;
+            // 
+            // Date
+            // 
+            this.Date.DataPropertyName = "Date";
+            this.Date.HeaderText = "Date";
+            this.Date.MinimumWidth = 6;
+            this.Date.Name = "Date";
+            this.Date.ReadOnly = true;
+            // 
+            // txtCaption
+            // 
+            this.txtCaption.Location = new System.Drawing.Point(105, 315);
+            this.txtCaption.Multiline = true;
+            this.txtCaption.Name = "txtCaption";
+            this.txtCaption.Size = new System.Drawing.Size(580, 72);
+            this.txtCaption.TabIndex = 7;
+            // 
+            // lblCaption
+            // 
+            this.lblCaption.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblCaption.Location = new System.Drawing.Point(36, 315);
+            this.lblCaption.Name = "lblCaption";
+            this.lblCaption.Size = new System.Drawing.Size(100, 19);
+            this.lblCaption.TabIndex = 8;
+            this.lblCaption.Text = "Caption:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(598, 394);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(87, 60);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Send";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.butSave_Click);
+            // 
+            // lblChannelID
+            // 
+            this.lblChannelID.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblChannelID.Location = new System.Drawing.Point(8, 397);
+            this.lblChannelID.Name = "lblChannelID";
+            this.lblChannelID.Size = new System.Drawing.Size(100, 19);
+            this.lblChannelID.TabIndex = 10;
+            this.lblChannelID.Text = "Channel ID:";
+            // 
+            // txtChannelID
+            // 
+            this.txtChannelID.Location = new System.Drawing.Point(105, 394);
+            this.txtChannelID.Name = "txtChannelID";
+            this.txtChannelID.Size = new System.Drawing.Size(487, 27);
+            this.txtChannelID.TabIndex = 11;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 427);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(87, 27);
+            this.btnBrowse.TabIndex = 17;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(105, 427);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(487, 27);
+            this.txtPath.TabIndex = 18;
+            // 
+            // frmBotManager
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(848, 515);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtChannelID);
+            this.Controls.Add(this.lblChannelID);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtCaption);
+            this.Controls.Add(this.dgvLog);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.lblToken);
+            this.Controls.Add(this.lblCaption);
+            this.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Name = "frmBotManager";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BotManager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmBotManager_FormClosing);
+            this.Load += new System.EventHandler(this.frmBotManager_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLog)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Label lblToken;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DataGridView dgvLog;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ChatID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Command;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MessageID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.TextBox txtCaption;
+        private System.Windows.Forms.Label lblCaption;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblChannelID;
+        private System.Windows.Forms.TextBox txtChannelID;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtPath;
+    }
+}
+
---
+++ BotManager/frmMain.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using MihaZupan;
+using Telegram.Bot.Types.ReplyMarkups;
+using System.IO;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.InputFiles;
+
+namespace BotManager
+{
+    public partial class frmBotManager : Form
+    {
+        private static string Token = "";
+        private Thread ThreadBot;
+        private Telegram.Bot.TelegramBotClient Bot;
+        BotManagerEntities db = new BotManagerEntities();
+
+        public string Link;
+        private string FileNameExtension = "";
+        OpenFileDialog File;
+        FileStream ImageStream = null;
+
+        public frmBotManager()
+        {
+            InitializeComponent();
+        }
+
+        private void runBot()
+        {
+            Bot = new Telegram.Bot.TelegramBotClient(Token);
+
+            this.Invoke(new Action(() =>
+            {
+                lblStatus.Text = "Online";
+                lblStatus.ForeColor = Color.Green;
+            }));
+            int offset = 0;
+
+            while (true)
+            {
+                try
+                {
+                    Telegram.Bot.Types.Update[] Update = Bot.GetUpdatesAsync(offset).Result;
+
+                    foreach (Telegram.Bot.Types.Update Up in Update)
+                    {
+                        offset = Up.Id + 1;
+                        while (Up.CallbackQuery != null)
+                        {
+                            //Contatct To Me Inline
+                            if (Up.CallbackQuery.Data.Contains("gmail")) { Bot.SendTextMessageAsync(Up.CallbackQuery.Message.Chat.Id, "My Gmail address is : hosseinshayesteh47.hs@gmail.com"); Up.CallbackQuery = null; break; }
+                            else if (Up.CallbackQuery.Data.Contains("outlook")) { Bot.SendTextMessageAsync(Up.CallbackQuery.Message.Chat.Id, "My Outlook address is : Shyestehhs@outlook.com"); Up.CallbackQuery = null; break; }
+                            //Poll Inline
+                            var user = db.PollManager.SingleOrDefault(u => u.UserID == Up.CallbackQuery.From.Id);
+                            if (user == null)
+                            {
+                                user = new PollManager()
+                                {
+                                    UserID = Up.CallbackQuery.From.Id,
+                                    VotedByUser = ""
+                                };
+                                db.PollManager.Add(user);
+                                db.SaveChanges();
+                            }
+                            var UserID = Up.CallbackQuery.From.Id;
+                            var pUserID = db.PollManager.Select(p => p.UserID).ToList();
+                            var pVotedByUSer = db.PollManager.Select(p => p.VotedByUser).ToList();
+                            var query = Up.CallbackQuery.Data;
+
+                            string VotedNow = Up.CallbackQuery.Data;
+                            if (db.PollManager.Any(p => (p.UserID == Up.CallbackQuery.From.Id) && (p.VotedByUser != VotedNow)))
+                            {
+                                Poll(Up, db.PollManager.SingleOrDefault(a => a.UserID == UserID).VotedByUser, VotedNow);
+
+                                InlineKeyboardMarkup PollButons = PollInline();
+                                Bot.EditMessageReplyMarkupAsync(Up.CallbackQuery.Message.Chat.Id, Up.CallbackQuery.Message.MessageId, replyMarkup: PollButons);
+                                Up.CallbackQuery = null;
+                            }
+                            else { Bot.SendTextMessageAsync(Up.CallbackQuery.Message.Chat.Id, "You have rated before"); }
+                            Up.CallbackQuery = null;
+                        }
+
+
+                        if (Up.Message == null) { continue; }
+
+                        string MessageFromUser = Up.Message.Text.ToLower();
+                        Telegram.Bot.Types.User Upfrom = Up.Message.From;
+                        Telegram.Bot.Types.ChatId ChatID = Up.Message.Chat.Id;
+
+                        StringBuilder SB = new StringBuilder();
+                        if (MessageFromUser.Contains("start"))
+                        {
+                            ReplyKeyboardMarkup MainKeyboardMarkup = StartKeyboards();
+                            SB.AppendLine("Welcome To My Bot");
+                            SB.AppendLine("Contact To Me : /ContactToMe");
+                            SB.AppendLine("About Me : /AboutMe");
+                            Bot.SendTextMessageAsync(ChatID, SB.ToString(), parseMode: default, false, true, 0, MainKeyboardMarkup);
+                        }
+                        else if (MessageFromUser.Contains("contacttome"))
+                        {
+                            ContactToMeKeyboards();
+                            SB.AppendLine("Please Choose one of this buttons");
+
+                            InlineKeyboardMarkup urlButons = new InlineKeyboardMarkup(new[]
+                            {
+                                new []
+                                {
+                                    InlineKeyboardButton.WithCallbackData("Gmail" + "\U0001F4E7" ,"gmail"),
+                                    InlineKeyboardButton.WithCallbackData("OutLook" + "\U0001F17E" ,"outlook"),
+                                },
+                                new[]
+                                {
+                                    InlineKeyboardButton.WithUrl("GitHub" + "\U0001F431" ,"https://github.com/ShayestehHS"),
+                                }
+                            });
+                            Bot.SendTextMessageAsync(ChatID, SB.ToString(), replyMarkup: urlButons);
+                        }
+                        else if (MessageFromUser.Contains("aboutme"))
+                        {
+                            ReplyKeyboardMarkup BackKeyboardMarkup = BackToFirst();
+                            SB.AppendLine("My name is Hossein Shayesteh");
+                            SB.AppendLine("and");
+                            SB.AppendLine("you can see my CV at my github page");
+                            SB.AppendLine("");
+                            SB.AppendLine("/ContactToMe");
+                            Bot.SendTextMessageAsync(ChatID, SB.ToString(), parseMode: default, false, true, 0, BackKeyboardMarkup);
+                        }
+                        else if (MessageFromUser.Contains("back"))
+                        {
+                            ReplyKeyboardMarkup MainKeyboardMarkup = BackToFirst();
+                            Bot.SendTextMessageAsync(ChatID, "/Back", parseMode: default, false, true, 0, MainKeyboardMarkup);
+                        }
+                        else if (MessageFromUser.Contains("poll"))
+                        {
+                            SB.AppendLine("Scale one to three,How you rate for my bot?");
+                            InlineKeyboardMarkup PollButons = PollInline();
+                            Bot.SendTextMessageAsync(ChatID, SB.ToString(), replyMarkup: PollButons);
+                        }
+
+                        this.Invoke(new Action(() =>
+                    {
+                        dgvLog.Rows.Add(ChatID, Up.Message.From.Username, Up.Message.Text, Up.Message.MessageId, DateTime.Now.ToString("dd - hh:mm:ss"));
+                    }));
+                    }
+                }
+                catch { MessageBox.Show("An error accured"); }
+            }
+        }
+
+        private void frmBotManager_Load(object sender, EventArgs e)
+        {
+            ThreadBot = new Thread(new ThreadStart(runBot));
+        }
+
+        //Keyboard and Inline Creators
+        private InlineKeyboardMarkup PollInline()
+        {
+            InlineKeyboardMarkup PollButons = new InlineKeyboardMarkup(new[]
+            {
+              new []
+              {
+                 InlineKeyboardButton.WithCallbackData("1 = Best" + $"({db.PollList.SingleOrDefault(a => a.PollTitle == "Best").NumbersOfVote})" ,"Best")
+              },
+              new[]
+               {
+                 InlineKeyboardButton.WithCallbackData("2 = Good" + $"({db.PollList.SingleOrDefault(a => a.PollTitle == "Good").NumbersOfVote})" ,"Good"),
+               },
+              new[]
+              {
+                 InlineKeyboardButton.WithCallbackData("3 = Awful" + $"({db.PollList.SingleOrDefault(a => a.PollTitle == "Awful").NumbersOfVote})" ,"Awful"),
+              }
+            });
+            return PollButons;
+        }
+
+        private ReplyKeyboardMarkup StartKeyboards()
+        {
+            /// Start keyboards
+            ReplyKeyboardMarkup MainKeyboardMarkup = new ReplyKeyboardMarkup();
+
+            KeyboardButton[] Start =
+            {
+              new  KeyboardButton("Start")
+            };
+
+            KeyboardButton[] Poll =
+            {
+             new KeyboardButton("Poll")
+            };
+            KeyboardButton[] AboutMe =
+            {
+              new KeyboardButton("AboutMe")
+            };
+            KeyboardButton[] ContactToMe =
+             {
+             new KeyboardButton("ContactToMe")
+            };
+
+            KeyboardButton[] MyCourse =
+            {
+             new KeyboardButton("ContactToMe")
+            };
+            KeyboardButton[] AllCourses =
+            {
+             new KeyboardButton("ContactToMe")
+            };
+
+            MainKeyboardMarkup.Keyboard = new KeyboardButton[][]
+            {
+                Start,
+                Poll,
+                AboutMe,
+                ContactToMe,
+            };
+            return MainKeyboardMarkup;
+        }
+
+        private ReplyKeyboardMarkup ContactToMeKeyboards()
+        {
+            ///Contact to me keyboards
+            ReplyKeyboardMarkup ContactKeyboardMarkup = new ReplyKeyboardMarkup();
+            KeyboardButton[] ContactRow1 =
+            {
+              new  KeyboardButton("Gamil")
+            };
+            KeyboardButton[] ContactRow2 =
+             {
+              new KeyboardButton("OutLook")
+            };
+            KeyboardButton[] ContactRow3 =
+             {
+             new KeyboardButton("GitHub")
+            };
+            KeyboardButton[] ContactRow4 =
+             {
+             new KeyboardButton("Show me all of this")
+            };
+            KeyboardButton[] Back =
+             {
+             new KeyboardButton("Back")
+            };
+            ContactKeyboardMarkup.Keyboard = new KeyboardButton[][]
+            {
+                ContactRow1,
+                ContactRow2,
+                ContactRow3,
+                ContactRow4,
+                Back
+            };
+            return ContactKeyboardMarkup;
+        }
+
+        private ReplyKeyboardMarkup BackToFirst()
+        {
+            ///Back to first keyboard
+            ReplyKeyboardMarkup BackKeyboardMarkup = new ReplyKeyboardMarkup();
+            KeyboardButton[] Back =
+             {
+             new KeyboardButton("Back")
+            };
+            BackKeyboardMarkup.Keyboard = new KeyboardButton[][]
+            {
+                Back
+            };
+            return BackKeyboardMarkup;
+        }
+
+        //Events
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Token = txtToken.Text;
+            ThreadBot.Start();
+            StartKeyboards();
+        }
+
+        private void frmBotManager_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ThreadBot.Abort();
+        }
+
+        private void butSave_Click(object sender, EventArgs e)
+        {
+            //
+            int IsValid = 0;
+
+            if (dgvLog.CurrentRow == null)
+            {
+                if (txtChannelID.Text == "")
+                {
+                    MessageBox.Show("Please select a row OR aad ChannelID");
+                }
+                else { IsValid = 1; }
+            }
+            else
+            {
+                IsValid = 2;
+            }
+
+
+            if (IsValid != 0)
+            {
+                ChatId ChatID;
+                if (IsValid == 1)
+                {
+                    ChatID = (ChatId)txtChannelID.Text;
+                }
+                else
+                {
+                    ChatID = (ChatId)dgvLog.CurrentRow.Cells[0].Value;
+                }
+                if (txtPath.Text == "")
+                {
+                    if (txtCaption.Text != null)
+                    {
+                        Bot.SendTextMessageAsync(ChatID, txtCaption.Text);
+                    }
+                    else { MessageBox.Show("Please fill the Caption TextBox"); }
+                }
+                else
+                {
+                    if (FileNameExtension.Contains(".jpg") || FileNameExtension.Contains(".png"))
+                    {
+                        Bot.SendPhotoAsync(ChatID, new InputOnlineFile(ImageStream, "1234.jpg"), txtCaption.Text);
+                    }
+                    else { Bot.SendPhotoAsync(ChatID, new InputOnlineFile(ImageStream, "1234.mp4"), txtCaption.Text); }
+                }
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            File = new OpenFileDialog();
+            if (File.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = File.FileName;
+                FileNameExtension = Path.GetExtension(txtPath.Text).ToString();
+                ImageStream = System.IO.File.Open(txtPath.Text, FileMode.Open);
+            }
+
+        }
+        //Other
+        private void Poll(Telegram.Bot.Types.Update Up, string VotedBefore, string VotedNow)
+        {
+            if (db.PollManager.Any(u => u.UserID == Up.CallbackQuery.From.Id))
+            {
+                db.PollList.SingleOrDefault(a => a.PollTitle == VotedBefore).NumbersOfVote -= 1;
+                db.PollList.SingleOrDefault(a => a.PollTitle == VotedNow).NumbersOfVote += 1;
+                db.PollManager.SingleOrDefault(a => a.UserID == Up.CallbackQuery.From.Id).VotedByUser = VotedNow;
+                db.SaveChanges();
+            }
+
+        }
+    }
+}
